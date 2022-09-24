615da91c6ad10fdb0f13c11fd7fc2b49a55c6476
REDMOND\shakulg <shakulg@microsoft.com>
2022-09-24 12:41:16 -0700

    fixed webpack with sourcemap

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import * as ReactDOM from 'react-dom/client';
 
 import App from "./components/app";
 
 
 // https://jscomplete.com/learn/1rd-reactful
+// $ npm run dev:server
+// $ npm run dev:bundler
+//
 const container = document.getElementById("app");
 ReactDOM.hydrateRoot(container, <App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  entry: `./src/index.jsx` ,
+  output: {
+    filename: "main.js"
+  },
+  devtool: 'eval-source-map',
   module: {
 
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$|jsx/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
